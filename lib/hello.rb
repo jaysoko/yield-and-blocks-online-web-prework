068b02ac90c744ed4bc992bef24c3c061b5cdbd7
Jay Sokolowski <jaysoko@gmail.com>
2019-02-07 02:29:41 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    if array.any? { |x| x.length > 3 }
+      array.each {|x| puts x.upcase }
+    else
+      array.each {|x| puts x.capitalize }
+    end
+  else puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
